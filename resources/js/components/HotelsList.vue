9df80ea831203087361bbdb30da5946a4de2473d
Eslam Elgamal <me@esl4m.com>
2019-06-25 23:26:29 +0300

    Adds vue & packages. Adds HotelsList component

---
+++ resources/js/components/HotelsList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="card">hello here from Vue</div>
+</template>
+
+<script>
+export default {
+  name: "HotelsList",
+  props: {},
+  mounted() {
+    console.log("Component mounted.");
+  }
+};
+</script>
