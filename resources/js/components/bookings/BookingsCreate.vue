0b381e3eb7d3b36df95d21a55f85b080beb59d69
Eslam Elgamal <me@esl4m.com>
2019-06-26 17:55:35 +0300

    Updates bookings create Vue

--- resources/js/components/bookings/BookingsCreate.vue
+++ resources/js/components/bookings/BookingsCreate.vue
@@ -1,49 +1,13 @@
 <template>
   <div class="card">
     <div class="card-header">
-      Manage Bookings
-      <a :href="'/bookings/create/'" class="btn btn-primary">New Booking</a>
+      New Booking
+      <a :href="'/bookings/'" class="btn btn-primary">List Bookings</a>
     </div>
 
     <div class="card-body">
       <div class="table-responsive">
-        <table class="table table-striped">
-          <thead>
-            <tr>
-              <td>ID</td>
-              <td>Hotel </td>
-              <td>Room</td>
-              <td>Start Date</td>
-              <td>End Date</td>
-              <td>Customer</td>
-              <td colspan="2">Actions</td>
-            </tr>
-          </thead>
 
-          <tbody>
-            <tr v-for="booking in bookings" :key="booking.id">
-              <td>
-                <a :href="'/bookings/'+ booking.id">{{booking.id}}</a>
-              </td>
-              <td>{{booking.hotel_id}}</td>
-              <td>{{booking.room_id}}</td>
-              <td>{{booking.start_date}}</td>
-              <td>{{booking.end_date}}</td>
-              <td>{{booking.customer_id}}</td>
-              <td>
-                <img src="#">
-              </td>
-              <td>
-                <a :href="'/bookings/edit/'+ booking.id"  class="btn btn-primary">Edit</a>
-              </td>
-              <td>
-                <form :action="'/bookings/destroy/'+ booking.id" method="post">
-                  <button class="btn btn-danger" type="submit">Delete</button>
-                </form>
-              </td>
-            </tr>
-          </tbody>
-        </table>
       </div>
     </div>
   </div>
@@ -51,7 +15,7 @@
 
 <script>
 export default {
-  name: "BookingsList",
+  name: "BookingsCreate",
   props: {
     bookings: {
       type: Array,
