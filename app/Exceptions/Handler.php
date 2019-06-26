a84dd995db2e01fc55c783a868421334ee2b1be1
Eslam Elgamal <me@esl4m.com>
2019-06-26 09:38:56 +0300

    Updates handler for 404 json response

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,13 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        // This will replace our 404 response with a JSON response.
+        if ($exception instanceof ModelNotFoundException && $request->wantsJson())
+        {
+            return response()->json([
+                'error' => 'Not found'
+            ], 404);
+        }
         return parent::render($request, $exception);
     }
 }
