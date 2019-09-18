10c0c1c0aab228634310a088a7867212c51176b4
Eslam Elgamal <me@esl4m.com>
2019-09-18 15:00:39 +0300

    Fix: Revert the default for days col. in migrations

--- database/migrations/2019_06_24_212636_create_bookings_table.php
+++ database/migrations/2019_06_24_212636_create_bookings_table.php
@@ -15,17 +15,13 @@ class CreateBookingsTable extends Migration
     {
         Schema::create('bookings', function (Blueprint $table) {
             $table->increments('id');
-
             $table->integer('hotel_id')->unsigned();
             $table->foreign('hotel_id')->references('id')->on('hotels')->onDelete('cascade')->onUpdate('cascade');
-
             $table->integer('room_id')->unsigned();
             $table->foreign('room_id')->references('id')->on('rooms')->onDelete('cascade')->onUpdate('cascade');
-
             $table->date('start_date');
             $table->date('end_date');
-            $table->integer('days')->default(0)->change();
-
+            $table->integer('days');
             $table->integer('customer_id')->unsigned();
             $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade')->onUpdate('cascade');
 
